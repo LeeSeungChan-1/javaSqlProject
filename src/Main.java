74de5c7e5c0d511f26b4cfa79eeaf869db6f2200
seung <seungchan753@naver.com>
2025-02-05 11:23:12 +0900

    feat db연결 확인

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.sql.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws SQLException {
+        String url = "jdbc:mysql://localhost:3306/db_javaSqlProject";
+        String user = "user_javaSqlProject";
+        String password = "1234";
+
+        Connection conn = DriverManager.getConnection(url, user, password);
+        Statement stmt = conn.createStatement();
+        ResultSet rs = stmt.executeQuery("select * from tbl_user");
+        while (rs.next()) {
+            int id = rs.getInt("id");
+            String name = rs.getString("name");
+            System.out.println("id: " + id + " name: " + name);
+        }
     }
 }
